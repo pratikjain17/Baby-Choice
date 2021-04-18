cd4655e05f1c4cac9114bfd993cda9612e1a82d6
Pratik <pratikmjain17@gmail.com>
2021-04-18 14:49:49 +0530

    payment api integration done

--- checkout.php
+++ checkout.php
@@ -7,101 +7,104 @@ include 'header.php';
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Baby Choice</title>
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/global.css" rel="stylesheet">
-	<link href="css/checkout.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
-	<link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
-	<script src="js/jquery-2.1.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Baby Choice</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/global.css" rel="stylesheet">
+  <link href="css/checkout.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+  <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+  <script src="js/jquery-2.1.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
 
-	<section id="center" class="clearfix checkout">
-		<div class="container">
-			<div class="row">
-				<div class="checkout_1 clearfix">
-					<div class="col-sm-6">
-						<div class="check_lm clearfix">
-							<div class="check_lm1 clearfix">
-								<ul class="mgt">
-									<li><a href="#">Cart</a> <i class="fa fa-chevron-right"></i></li>
-									<li>Information <i class="fa fa-chevron-right"></i></li>
-									<li>Shipping <i class="fa fa-chevron-right"></i></li>
-									<li>Payment</li>
-								</ul>
-							</div><br>
-							<div class="check_lm2 clearfix">
-								<div class="col-sm-6 space_left">
-									<h4>Contact information</h4>
-								</div>
-								<div class="col-sm-6 space_left">
-									<p>Already have an account? Already have an account? <a class="bold" href="#">Log in</a></p>
-								</div>
-							</div>
-							<div class="check_lm3 clearfix">
-								<input class="form-control" placeholder="Email" type="text">
-								<h5 class="normal"><input class="check_1" type="checkbox"> <span>Keep me up to date on news and exclusive offers</span></h5><br>
-								<h4>Shipping address</h4>
-							</div>
-							<div class="check_lm4 clearfix">
-								<div class="col-sm-6 space_left">
-									<input class="form-control" placeholder="First Name" type="text">
-								</div>
-								<div class="col-sm-6 space_right">
-									<input class="form-control" placeholder="Last Name" type="text">
-								</div>
-							</div>
-							<div class="check_lm4 clearfix">
-								<input class="form-control" placeholder="Address" type="text">
-								<input class="form-control" placeholder="House No" type="text">
-								<input class="form-control" placeholder="City" type="text">
-							</div>
-							<div class="check_lm4 clearfix">
-								<div class="col-sm-4 space_left">
-									<select class="form-control" id="subject" name="subject">
-										<option>Country</option>
-										<option>India</option>
-										<option>America</option>
-										<option>Afg</option>
-										<option>Ban</option>
-									</select>
-								</div>
-								<div class="col-sm-4 space_all">
-									<select class="form-control" id="subject" name="subject">
-										<option>State</option>
-										<option>Maharashtra</option>
-										<option>UP</option>
-										<option>MP</option>
-										<option>Delhi</option>
-										<option>Bengal</option>
-									</select>
-								</div>
-								<div class="col-sm-4 space_right">
-									<input class="form-control" placeholder="Pin Code" type="text">
-								</div>
-							</div>
-							<div class="check_lm5 clearfix">
-								<h5 class="normal"><input class="check_1" type="checkbox"> <span>Save this information for next time</span></h5>
-							</div>
-							<div class="checkout_1l3 clearfix">
-								<hr>
-								<div class="col-sm-6">
-									<h5 class="mgt"><a href=" cart.php?usercart=<?php echo $_SESSION['userid'] ?>"><i class="fa fa-chevron-left"></i> Return to cart</a></h5>
-								</div>
-								<div class="col-sm-6">
-									<h5 class="mgt text-right"><a class="button mgt">Continue</a></h5>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<?php
+  <section id="center" class="clearfix checkout">
+    <div class="container">
+      <div class="row">
+        <div class="checkout_1 clearfix">
+          <div class="col-sm-6">
+            <div class="check_lm clearfix">
+              <div class="check_lm1 clearfix">
+                <ul class="mgt">
+                  <li><a href="#">Cart</a> <i class="fa fa-chevron-right"></i></li>
+                  <li>Information <i class="fa fa-chevron-right"></i></li>
+                  <li>Shipping <i class="fa fa-chevron-right"></i></li>
+                  <li>Payment</li>
+                </ul>
+              </div><br>
+              <div class="check_lm2 clearfix">
+                <div class="col-sm-6 space_left">
+                  <h4>Contact information</h4>
+                </div>
+                <div class="col-sm-6 space_left">
+                  <p>Already have an account? Already have an account? <a class="bold" href="#">Log in</a></p>
+                </div>
+              </div>
+              <div class="check_lm3 clearfix">
+                <input class="form-control" placeholder="Email" type="text">
+                <h5 class="normal"><input class="check_1" type="checkbox"> <span>Keep me up to date on news and
+                    exclusive offers</span></h5><br>
+                <h4>Shipping address</h4>
+              </div>
+              <div class="check_lm4 clearfix">
+                <div class="col-sm-6 space_left">
+                  <input class="form-control" placeholder="First Name" type="text">
+                </div>
+                <div class="col-sm-6 space_right">
+                  <input class="form-control" placeholder="Last Name" type="text">
+                </div>
+              </div>
+              <div class="check_lm4 clearfix">
+                <input class="form-control" placeholder="Address" type="text">
+                <input class="form-control" placeholder="House No" type="text">
+                <input class="form-control" placeholder="City" type="text">
+              </div>
+              <div class="check_lm4 clearfix">
+                <div class="col-sm-4 space_left">
+                  <select class="form-control" id="subject" name="subject">
+                    <option>Country</option>
+                    <option>India</option>
+                    <option>America</option>
+                    <option>Afg</option>
+                    <option>Ban</option>
+                  </select>
+                </div>
+                <div class="col-sm-4 space_all">
+                  <select class="form-control" id="subject" name="subject">
+                    <option>State</option>
+                    <option>Maharashtra</option>
+                    <option>UP</option>
+                    <option>MP</option>
+                    <option>Delhi</option>
+                    <option>Bengal</option>
+                  </select>
+                </div>
+                <div class="col-sm-4 space_right">
+                  <input class="form-control" placeholder="Pin Code" type="text">
+                </div>
+              </div>
+              <div class="check_lm5 clearfix">
+                <h5 class="normal"><input class="check_1" type="checkbox"> <span>Save this information for next
+                    time</span></h5>
+              </div>
+              <div class="checkout_1l3 clearfix">
+                <hr>
+                <div class="col-sm-6">
+                  <h5 class="mgt"><a href=" cart.php?usercart=<?php echo $_SESSION['userid'] ?>"><i
+                        class="fa fa-chevron-left"></i> Return to cart</a></h5>
+                </div>
+                <div class="col-sm-6">
+                  <h5 class="mgt text-right"><a class="button mgt" href="payment.php">Continue</a></h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <?php
 						if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 							$user_id = $_SESSION['userid'];
 							$sql1 = "SELECT * FROM `cart` WHERE `user_id` = $user_id";
@@ -124,22 +127,22 @@ include 'header.php';
 								<div class="checkout_1r mgt clearfix">
 									<div class="checkout_1l2 mgt clearfix">
 										<div class="col-sm-2">
-											<img src="img/'.$product_image.'" alt="abc" class="iw">
+											<img src="img/' . $product_image . '" alt="abc" class="iw">
 										</div>
 										<div class="col-sm-7">
-											<p class="mgt">'.$product_name.'</p>
-											<h6>Quantity : '.$cartquantity.'</h6>
+											<p class="mgt">' . $product_name . '</p>
+											<h6>Quantity : ' . $cartquantity . '</h6>
 										</div>
 										<div class="col-sm-3">
-											<h6 class="text-right col_3">'.$product_discountprice.'</h6>
+											<h6 class="text-right col_3">' . $product_discountprice . '</h6>
 										</div>
 									</div>
 								</div>';
 							}
 						}
 						?>
-						
-							<!-- <div class="checkout_1r clearfix">
+
+            <!-- <div class="checkout_1r clearfix">
 								<div class="checkout_1l2 clearfix">
 									<div class="col-sm-2">
 										<img src="img/21.jpg" alt="abc" class="iw">
@@ -181,37 +184,38 @@ include 'header.php';
 									</div>
 								</div>
 							</div> -->
-							<div class="checkout_1r1 clearfix">
-								<hr>
-								<div class="checkout_1r1i clearfix">
-									<div class="col-sm-6">
-										<h5 class="mgt normal">Subtotal</h5>
-										<h5 class="normal">Shipping</h5>
-									</div>
-									<div class="col-sm-6">
-										<h5 class="mgt col_3">Rs.<?php echo $_SESSION['cartBill'] ?></h5>
-										<h6>Calculated at next step</h6>
-									</div>
-								</div>
-							</div> 
-							<div class="checkout_1r1 clearfix">
-								<hr>
-								<div class="checkout_1r1i clearfix">
-									<div class="col-sm-6">
-										<h5 class="mgt normal">Total</h5>
-									</div>
-									<div class="col-sm-6">
-										<h4 class="mgt"><span class="normal">INR</span> <span class="col_3">Rs.<?php echo $_SESSION['cartBill']+60 ?></span></h4>
-									</div>
-								</div>
-							</div> 
-						</div>
-					</div>
-				</div>
+            <div class="checkout_1r1 clearfix">
+              <hr>
+              <div class="checkout_1r1i clearfix">
+                <div class="col-sm-6">
+                  <h5 class="mgt normal">Subtotal</h5>
+                  <h5 class="normal">Shipping</h5>
+                </div>
+                <div class="col-sm-6">
+                  <h5 class="mgt col_3">Rs.<?php echo $_SESSION['cartBill'] ?></h5>
+                  <h6>Calculated at next step</h6>
+                </div>
+              </div>
+            </div>
+            <div class="checkout_1r1 clearfix">
+              <hr>
+              <div class="checkout_1r1i clearfix">
+                <div class="col-sm-6">
+                  <h5 class="mgt normal">Total</h5>
+                </div>
+                <div class="col-sm-6">
+                  <h4 class="mgt"><span class="normal">INR</span> <span
+                      class="col_3">Rs.<?php echo $_SESSION['cartBill'] + 60 ?></span></h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			</div>
-		</div>
-	</section>
+    </div>
+    </div>
+  </section>
 
 </body>
 
---
+++ payment.php
@@ -0,0 +1,86 @@
+<?php
+header("Pragma: no-cache");
+header("Cache-Control: no-cache");
+header("Expires: 0");
+include 'db_connection.php';
+include 'header.php';
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Baby Choice</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/global.css" rel="stylesheet">
+<link href="css/index.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+<link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+<script src="js/jquery-2.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+
+<head>
+  <title>Merchant Check Out Page</title>
+  <meta name="GENERATOR" content="Evrsoft First Page">
+</head>
+
+<body>
+  <h1>Merchant Check Out Page</h1>
+  <pre>
+	</pre>
+  <form method="post" action="pgRedirect.php">
+    <table border="1">
+      <tbody>
+        <tr>
+          <th>S.No</th>
+          <th>Label</th>
+          <th>Value</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td><label>ORDER_ID::*</label></td>
+          <td><input id="ORDER_ID" tabindex="1" maxlength="20" size="20" name="ORDER_ID" autocomplete="off"
+              value="<?php echo  "ORDS" . rand(10000, 99999999) ?>">
+          </td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td><label>CUSTID ::*</label></td>
+          <td><input id="CUST_ID" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off"
+              value="CUST001"></td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td><label>INDUSTRY_TYPE_ID ::*</label></td>
+          <td><input id="INDUSTRY_TYPE_ID" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID"
+              autocomplete="off" value="Retail"></td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td><label>Channel ::*</label></td>
+          <td><input id="CHANNEL_ID" tabindex="4" maxlength="12" size="12" name="CHANNEL_ID" autocomplete="off"
+              value="WEB">
+          </td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td><label>txnAmount*</label></td>
+          <td><input title="TXN_AMOUNT" tabindex="10" type="text" name="TXN_AMOUNT"
+              value="<?php echo $_SESSION['cartBill'] ?>">
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td><input value="CheckOut" type="submit" onclick=""></td>
+        </tr>
+      </tbody>
+    </table>
+    * - Mandatory Fields
+  </form>
+</body>
+
+</html>
